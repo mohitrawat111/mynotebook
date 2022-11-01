c7420697696caa3320ba8652ab03695f5abf2ae8
mohitrawat111 <109884777+mohitrawat111@users.noreply.github.com>
2022-11-01 18:23:27 +0530

    backend changes for heroku....

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,15 @@
+
 const moongose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/mynotebook"
+
+// const mongoURI = "mongodb://localhost:27017/mynotebook"
+// const mongoURI = 'mongodb+srv://mynotebook:yuZVDTLFNKW2c8ah@cluster0.ubmgnxz.mongodb.net/mynotebook?retryWrites=true&w=majority'
+const mongoURI = process.env.DATABASE
 
 const connectToMongo = () => {
     moongose.connect(mongoURI, () => {
         console.log("connect to Mongo Succesfully");
+
     })
 }
 
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors')
@@ -7,7 +8,7 @@ var cors = require('cors')
 connectToMongo();
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
